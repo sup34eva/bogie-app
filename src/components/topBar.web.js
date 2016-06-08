a8842b8e770debb747d7a61444362b8b119947e1
Sboob <antho10440@hotmail.fr>
2016-06-08 19:34:18 +0200

    TopBar

--- src/components/topBar.web.js
+++ src/components/topBar.web.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import cookie from 'react-cookie';
 import {
     View,
     Text,
     StyleSheet
 } from 'react-native';
+import {
+    Link
+} from 'react-router';
 
 const styles = StyleSheet.create({
     topBar: {
@@ -32,10 +36,15 @@ const styles = StyleSheet.create({
 export default function () {
     return (
         <View style={styles.topBar}>
-            <Text style={styles.p}>Home</Text>
-            <Text style={styles.p}>Reserve</Text>
-            <Text style={styles.p}>Login</Text>
-            <Text style={styles.p}>Register</Text>
+            <Link to="/" style={styles.p}>Home</Link>
+            {cookie.load('token') ? [
+                <Link key="profile" to="/profile" style={styles.p}>Profile</Link>,
+                <Link key="logout" to="/logout" style={styles.p}>Logout</Link>
+            ] : [
+                <Link key="login" to="/login" style={styles.p}>Login</Link>,
+                <Link key="register" to="/register" style={styles.p}>Register</Link>
+            ]}
+
         </View>
     );
 }
