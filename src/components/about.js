1586401117b114d235487fd598a0fde29a7e0a0e
thalapathy <jesanth@hotmail.fr>
2016-06-08 11:59:50 +0200

    Page about | Fixed Prototype error(field)

---
+++ src/components/about.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    Platform,
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+import Relay from 'react-relay';
+
+export default class about extends React.Component {
+    static propTypes = {
+        onConnect: React.PropTypes.func,
+        style: View.propTypes.style
+    };
+    render() {
+        return (
+
+
+<View>
+    <Text>
+            ta mere
+    </Text>
+</View>
+
+);
+}
+}
