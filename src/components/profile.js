7555c772b981c8ea4acbbc94cc9653cf311f4784
thalapathy <jesanth@hotmail.fr>
2016-06-10 01:07:08 +0200

    Profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,31 +7,83 @@ import Relay from 'react-relay';
 import Field from './base/field';
 import Button from './base/button';
 import ListView from './base/ListView';
+import cookie from 'react-cookie';
+
 
 import EditMail from '../mutations/editMail';
 import EditPassword from '../mutations/editPassword';
 
 class Profile extends React.Component {
     static propTypes = {
+        viewer: React.PropTypes.object,
         onConnect: React.PropTypes.func,
         style: View.propTypes.style
     };
 
-constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.state = {
-        email: null,
-        password: ''
-    };
-}
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            error: null,
+            email: null,
+            newPassword: '',
+            passwordConfirmation: '',
+            currentPassword: ''
+        };
+    }
 
-onSubmit() {
-    Relay.Store.commitUpdate(new editPassword({
-        currentPassword: this.state.currentPassword,
-        passwordLink: this.state.passwordLink
-    }), this.onToken());
-}
+    onSubmit() {
+        if (this.state.currentPassword.length > 0 && (this.state.newPassword.length === 0 || this.state.passwordConfirmation.length === 0)) {
+            this.setState({
+                error: 'Type your new password'
+            });
+        } else if (this.state.currentPassword.length === 0 && this.state.newPassword.length > 0 && this.state.newPassword === this.state.passwordConfirmation) {
+            this.setState({
+                error: 'Type your current password'
+            });
+        } else if (this.state.currentPassword.length > 0 && this.state.newPassword.length > 0 && this.state.newPassword !== this.state.passwordConfirmation) {
+            this.setState({
+                error: 'The passwords do not match'
+            });
+        } else if (this.state.currentPassword.length > 0 && this.state.newPassword.length > 0 && this.state.newPassword === this.state.passwordConfirmation) {
+            Relay.Store.commitUpdate(new EditPassword({
+                currentPassword: this.state.currentPassword,
+                newPassword: this.state.newPassword,
+                token: cookie.load('token')
+            }), {
+                onSuccess: data => {
+                    cookie.save('token', data.editPassword.token);
+                },
+                onFailure: transaction => {
+                    const {
+                        source: {
+                            errors: [err]
+                        }
+                    } = transaction.getError();
+                    console.error(err);
+                }
+            });
+        }
+        if (this.state.email !== null) {
+            Relay.Store.commitUpdate(new EditMail({
+                currentPassword: this.state.email,
+                token: cookie.load('token')
+            }), {
+                onSuccess: () => {
+                    console.log('Okçamarche');
+                },
+                onFailure: transaction => {
+                    const {
+                        source: {
+                            errors: [err]
+                        }
+                    } = transaction.getError();
+                    console.error(err);
+                }
+            });
+        }
+    }
+
     render() {
         const emailLink = {
             value: this.state.email === null ? this.props.viewer.me.email : this.state.email,
@@ -58,7 +110,7 @@ onSubmit() {
             requestChange: currentPassword => {
                 this.setState({currentPassword});
             }
-        }
+        };
 
         return (
             <View>
@@ -67,13 +119,14 @@ onSubmit() {
                     <Field type="password" name="Current Password" valueLink={currentPassword} />
                     <Field type="password" name="Password" valueLink={passwordLink} />
                     <Field type="password" name="Password Confirmation" valueLink={passwordConfLink} />
+                    {this.state.error && <Text>{this.state.error}</Text>}
                     <Button onPress={this.onSubmit}>
                         <Text>Submit</Text>
                     </Button>
                 </View>
                 <View>
                     <Text>Historique de la commande</Text>
-                    <ListView dataSource={this.props.viewer.history.edges} renderRow={edge => <History key={edge.node.id} history={edge.node} />}/>
+                    <ListView dataSource={this.props.viewer.me.history.edges} renderRow={edge => <History key={edge.node.id} history={edge.node} />}/>
                 </View>
             </View>
         );
